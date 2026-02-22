88df31feb16f2598ed7feba3fd32738a407e1711
9duc_nguyenminh <9duc_nguyenminh@singaporeair.com.sg>
2026-02-22 15:28:28 +0700

    re-design home screen

--- app/src/main/java/com/loud/mytestapplication/ui/home/HomeScreen.kt
+++ app/src/main/java/com/loud/mytestapplication/ui/home/HomeScreen.kt
@@ -1,17 +1,31 @@
 package com.loud.mytestapplication.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.loud.mytestapplication.R
 import com.loud.mytestapplication.effect.Effect
 
 @Composable
@@ -46,11 +60,47 @@ fun HomeScreenContent(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Button(onPokerClick) {
-            Text(text = "Poker")
+        Button(
+            onPokerClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            ),
+            contentPadding = PaddingValues(
+                horizontal = 16.dp,
+                vertical = 2.dp
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_game),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(24.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(R.string.poker), fontSize = 14.sp)
         }
-        Button(onAnalyticsClick) {
-            Text(text = "Analytics")
+        Button(
+            onAnalyticsClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            ),
+            contentPadding = PaddingValues(
+                horizontal = 16.dp,
+                vertical = 2.dp
+            )
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_chart),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(24.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(R.string.analytics), fontSize = 14.sp)
         }
     }
 }
