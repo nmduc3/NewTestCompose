e33cba5819c5196a000659403b4c8d0199a23065
9duc_nguyenminh <9duc_nguyenminh@singaporeair.com.sg>
2026-02-22 16:18:04 +0700

    set full screen and system bar color

--- app/src/main/java/com/loud/mytestapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/loud/mytestapplication/ui/main/MainActivity.kt
@@ -2,7 +2,9 @@ package com.loud.mytestapplication.ui.main
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.loud.mytestapplication.graph.AppNavGraph
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +13,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // change status bar color
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = false // Icon light
+            isAppearanceLightNavigationBars = false // Navigation bar icon light
+        }
         setContent {
             val navController = rememberNavController()
             AppNavGraph(navController = navController)
